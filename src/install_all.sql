d8b67f0251c0abaea6e9886195c1eda5f60125e2
commi235 <moritz.klein@it-twins.de>
2020-06-30 19:52:02 +0200

    separate db objects installation from app

--- src/install_all.sql
+++ src/install_all.sql
@@ -8,28 +8,7 @@ ACCEPT parsing_schema char default 'FLOWS4APEX' PROMPT 'Enter Parsing Schema: '
 ACCEPT app_alias char default 'F4A' PROMPT 'Enter Application Alias: '
 
 
-PROMPT >> Database Objects Installation
-PROMPT >> =============================
-
-PROMPT >> Installing Tables
-@ddl/install_tables.sql
-
-PROMPT >> Installing Package Specifications
-@plsql/flow_api_pkg.pks
-@plsql/flow_bpmn_parser_pkg.pks
-
-PRoMPT >> Installing Package Bodies
-@plsql/flow_api_pkg.pkb
-@plsql/flow_bpmn_parser_pkg.pkb
-
-PROMPT >> installing Views
-@views/flow_p0001_vw.sql
-@views/flow_p0003_vw.sql
-@views/flow_p0010_vw.sql
-@views/flow_p0010_instances_vw.sql
-@views/flow_p0010_subflows_vw.sql
-@views/flow_p0010_branches_vw.sql
-@views/flow_dgrm_lov.sql
+@install_db.sql
 
 PROMPT >> Application Installation
 PROMPT >> ========================
