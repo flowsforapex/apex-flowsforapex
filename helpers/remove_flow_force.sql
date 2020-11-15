126275f3ec18efec20bbf615760ce24ce4f143dd
commi235 <moritz.klein@it-twins.de>
2020-11-15 20:56:49 +0100

    fix prefix

--- helpers/remove_flow_force.sql
+++ helpers/remove_flow_force.sql
@@ -7,7 +7,7 @@ PROMPT >> Removing Views
 
 begin
 
-  for rec in ( select object_name from user_objects where object_type = 'PACKAGE' and object_name like 'FLOWS\_%' escape '\' )
+  for rec in ( select object_name from user_objects where object_type = 'PACKAGE' and object_name like 'FLOW\_%' escape '\' )
   loop
     execute immediate 'drop package ' || rec.object_name;
   end loop;
@@ -18,7 +18,7 @@ end;
 
 PROMPT >> Removing Packages
 begin
-  for rec in ( select view_name from user_views where view_name like 'FLOWS\_%' escape '\' )
+  for rec in ( select view_name from user_views where view_name like 'FLOW\_%' escape '\' )
   loop
     execute immediate 'drop view ' || rec.view_name;
   end loop;
@@ -27,7 +27,7 @@ end;
 
 PROMPT >> Removing Tables
 begin
-  for rec in ( select table_name from user_tables where table_name like 'FLOWS\_%' escape '\' )
+  for rec in ( select table_name from user_tables where table_name like 'FLOW\_%' escape '\' )
   loop
     execute immediate 'drop table ' || rec.table_name || ' cascade constraints';
   end loop;
