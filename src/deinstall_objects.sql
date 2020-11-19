7f3240e8403879e4ac4b1a43288c41123f687180
commi235 <moritz.klein@it-twins.de>
2020-11-19 09:53:23 +0100

    include missing view in uninstall

--- src/deinstall_objects.sql
+++ src/deinstall_objects.sql
@@ -22,6 +22,7 @@ drop view flow_p0010_branches_vw;
 drop view flow_p0010_instances_vw;
 drop view flow_p0010_subflows_vw;
 drop view flow_p0010_routes_vw;
+drop view flow_p0010_variables_vw;
 drop view flow_task_inbox_vw;
 drop view flow_instance_details_vw;
 drop view flow_instance_variables_vw;
