7d1e826462d64ce27d4f2340934cb219548189ba
Richard Allen <rallen2010@gmail.com>
2020-12-07 15:21:20 +0000

    adds F4A tutorial diagrams and installation

---
+++ bpmn_tutorials/install_tutorials.sql
@@ -0,0 +1,16 @@
+PROMPT >> Installing Tutorial Diagrams
+PROMPT >> ===========================
+
+@sql/f4ATutorial0.sql
+@sql/f4ATutorial1.sql
+@sql/f4ATutorial2.sql
+@sql/f4ATutorial3.sql
+@sql/f4ATutorial4.sql
+@sql/f4ATutorial5.sql
+@sql/f4ATutorial6.sql
+@sql/f4ATutorial7.sql
+@sql/f4ATutorial8.sql
+
+
+PROMPT >> Tutorial Diagrams Installation finished
+PROMPT >> ======================================
