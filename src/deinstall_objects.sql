1eda94e1f26ff8034b4568cba524c5d97cfd5520
Richard Allen <rallen2010@gmail.com>
2020-12-21 14:43:33 +0000

    versioning - first cut db changes

--- src/deinstall_objects.sql
+++ src/deinstall_objects.sql
@@ -30,6 +30,7 @@ drop view flow_instance_details_vw;
 drop view flow_instance_variables_vw;
 drop view flow_instances_vw;
 drop view flow_subflows_vw;
+drop view flow_diagram_categories_lov;
 drop view flow_diagrams_lov;
 drop view flow_diagrams_parsed_lov;
 
