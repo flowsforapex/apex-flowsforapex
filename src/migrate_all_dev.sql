789f1bbf442b72bf85e459ad9000261b3f19731b
commi235 <moritz.klein@it-twins.de>
2021-02-05 13:30:26 +0100

    make migration scripts more flexible scripts in folder ddl should name from and to version Calling scripts get two mandatory parameters

--- src/migrate_all_dev.sql
+++ src/migrate_all_dev.sql
@@ -10,9 +10,11 @@ ACCEPT ws_name char default 'FLOWS4APEX' PROMPT 'Enter Workspace Name: [FLOWS4AP
 ACCEPT parsing_schema char default 'FLOWS4APEX' PROMPT 'Enter Parsing Schema: [FLOWS4APEX]'
 ACCEPT app_alias char default 'FLOWS4APEX' PROMPT 'Enter Application Alias: [FLOWS4APEX]'
 ACCEPT app_name char default 'Flows for APEX' PROMPT 'Enter Application Name: [Flows for APEX]'
+ACCEPT from_version char PROMPT 'Enter current installed release:'
+ACCEPT to_version char PROMPT 'Enter next release to upgrade to:'
 
 
-@migrate_db.sql
+@migrate_db.sql ^from_version. ^to_version.
 
 PROMPT >> Application Installation
 PROMPT >> ========================
