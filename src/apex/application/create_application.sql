6a96de9ee55542e637a8cc8c2a2a8398e420ab22
Louis Moreaux <louis@agilab.com>
2021-02-15 18:04:40 +0100

    Engine-app updates

--- src/apex/application/create_application.sql
+++ src/apex/application/create_application.sql
@@ -52,7 +52,7 @@ wwv_flow_api.create_flow(
 ,p_substitution_string_01=>'APP_NAME'
 ,p_substitution_value_01=>'Workflow'
 ,p_last_updated_by=>'FLOWS4APEX'
-,p_last_upd_yyyymmddhh24miss=>'20210213085108'
+,p_last_upd_yyyymmddhh24miss=>'20210215180253'
 ,p_file_prefix => nvl(wwv_flow_application_install.get_static_app_file_prefix,'')
 ,p_files_version=>19
 ,p_ui_type_name => null
