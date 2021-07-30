16ce38481e66ec8057a53be0e31890e90d78a52a
Louis Moreaux <moreaux.louis@gmail.com>
2021-07-30 10:14:48 +0200

    Update flow_p0002_diagrams_vw.sql
    Remove HTML button from that view as it is not needed

--- src/views/engine-app/flow_p0002_diagrams_vw.sql
+++ src/views/engine-app/flow_p0002_diagrams_vw.sql
@@ -7,10 +7,9 @@ as
        , d.dgrm_status
        , d.dgrm_category
        , d.dgrm_last_update
-       , '<button type="button" title="Edit" aria-label="Edit" class="t-Button t-Button--noLabel t-Button--icon" onclick="location.href='''||apex_page.get_url(p_page => 7, p_items => 'P7_DGRM_ID', p_values => d.dgrm_id)||'''"><span aria-hidden="true" class="t-Icon fa fa-pencil"></span></button>'||
-         '<button type="button" title="New Version" aria-label="New Version" class="clickable-action t-Button t-Button--noLabel t-Button--icon" data-dgrm="' || d.dgrm_id || '" data-action="new_version"><span aria-hidden="true" class="t-Icon fa fa-arrow-circle-o-up"></span></button>'||
-         '<button type="button" title="Create instance" aria-label="Create instance" class="t-Button t-Button--noLabel t-Button--icon" onclick="'||apex_page.get_url(p_page => 11, p_items => 'P11_DGRM_ID', p_values => d.dgrm_id)||'"><span aria-hidden="true" class="t-Icon fa fa-plus"></span></button>'
-         as btn
+       , null as btn
+       , apex_page.get_url(p_page => 7, p_items => 'P7_DGRM_ID', p_values => d.dgrm_id) as edit_link
+       , apex_page.get_url(p_page => 11, p_items => 'P11_DGRM_ID', p_values => d.dgrm_id) as create_instance_link
        , coalesce( (select count(*) from flow_instances_vw i where i.dgrm_id = d.dgrm_id), 0 ) as instances 
        , case when coalesce( (select count(*) from flow_objects objt where objt.objt_dgrm_id = d.dgrm_id), 0 ) = 0 then 'No' else 'Yes' end as diagram_parsed
        , case when coalesce( (select count(*) from flow_objects objt where objt.objt_dgrm_id = d.dgrm_id), 0 ) = 0 then 'fa-times-circle-o fa-lg u-danger-text' else 'fa-check-circle-o fa-lg u-success-text' end as diagram_parsed_icon
