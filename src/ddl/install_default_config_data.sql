75afaee0e9d926d41d452ee2361fed7b2d0f5ad9
Richard Allen <rallen2010@gmail.com>
2021-08-24 15:28:42 +0100

    fix table name in config data

--- src/ddl/install_default_config_data.sql
+++ src/ddl/install_default_config_data.sql
@@ -1,3 +1,3 @@
-insert into flow_configurations (cfig_key, cfig_value) values ('logging_level', 'standard');
-insert into flow_configurations (cfig_key, cfig_value) values ('logging_hide_userid', 'false');
+insert into flow_configuration (cfig_key, cfig_value) values ('logging_level', 'standard');
+insert into flow_configuration (cfig_key, cfig_value) values ('logging_hide_userid', 'false');
 
