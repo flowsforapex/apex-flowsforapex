c9ead7d0dd5a274d98ba7ee51461327e7799f0fe
Louis Moreaux <moreaux.louis@gmail.com>
2021-09-16 17:14:44 +0200

    Add fr file for french translation

--- src/data/install_engine_messages_fr.sql
+++ src/data/install_engine_messages_fr.sql
@@ -1,179 +1,176 @@
--- delete from flow_messages;
-
-
 insert into flow_messages( fmsg_message_key, fmsg_lang, fmsg_message_content )
  values (
 'eng_handle_event_int',
-'en',
-'Flow Engine Internal Error: Process %0 Subflow %1 Module %2 Current %4 Current Tag %3'
-);
- 
-insert into flow_messages( fmsg_message_key, fmsg_lang, fmsg_message_content )
- values (
-'more_than_1_forward_path',
-'en',
-'More than 1 forward path found when only 1 allowed.'
-);
- 
-insert into flow_messages( fmsg_message_key, fmsg_lang, fmsg_message_content )
- values (
-'no_next_step_found',
-'en',
-'No Next Step Found on subflow %0.  Check your process diagram.'
-);
- 
-insert into flow_messages( fmsg_message_key, fmsg_lang, fmsg_message_content )
- values (
-'plsql_script_failed',
-'en',
-'Process %0: ScriptTask %1 failed due to PL/SQL error - see event log.'
+'fr',
+'Erreur interne du moteur de flux: Processus %0 Sous-flux %1 Module %2 Courant %4 Balise courante %3'
 );
 
 insert into flow_messages( fmsg_message_key, fmsg_lang, fmsg_message_content )
  values (
+'more_than_1_forward_path',
+'fr',
+'Plus d''un chemin d''accès trouvé alors qu''un seul est autorisé.'
+);
+
+insert into flow_messages( fmsg_message_key, fmsg_lang, fmsg_message_content )
+ values (
+'no_next_step_found',
+'fr',
+'Aucune étape suivante trouvée pour le sous-flux %0. Vérifiez votre diagramme de processus.'
+);
+
+insert into flow_messages( fmsg_message_key, fmsg_lang, fmsg_message_content )
+ values (
+'plsql_script_failed',
+'fr',
+'Processus %0 : Tâche de script %1 a échoué en raison d''une erreur PL/SQL - voir le journal des événements.'
+);
+ 
+insert into flow_messages( fmsg_message_key, fmsg_lang, fmsg_message_content )
+ values (
 'plsql_script_requested_stop',
-'en',
-'Process %0: ScriptTask %1 requested processing stop - see event log.'
+'fr',
+'Processus %0: Tâche de script %1 arrêt du traitement demandé - voir le journal des événements.'
 );
  
 insert into flow_messages( fmsg_message_key, fmsg_lang, fmsg_message_content )
  values (
 'timeout_locking_subflow',
-'en',
-'Unable to lock Subflow : %0.'
+'fr',
+'Impossible de verrouiller le sous-flux : %0.'
 );
  
 insert into flow_messages( fmsg_message_key, fmsg_lang, fmsg_message_content )
  values (
 'timer_broken',
-'en',
-'Timer %0 broken in process %1 , subflow : %2.  See error_info.'
+'fr',
+'Minuteur %0 en erreur dans le processus %1 , sous-flux : %2.  Voir error_info.'
 );
  
 insert into flow_messages( fmsg_message_key, fmsg_lang, fmsg_message_content )
  values (
 'var_exp_plsql_error',
-'en',
-'Subflow : %0 Error in %2 expression for Variable : %1'
+'fr',
+'Sous-flux : %0 Erreur dans l''expression %2 de la variable : %1'
 );
  
 insert into flow_messages( fmsg_message_key, fmsg_lang, fmsg_message_content )
  values (
 'var_exp_sql_no_data',
-'en',
-'Error setting %2 process variable %1 in process id %0.  No data found in query.'
+'fr',
+'Ereur lors de la mise à jour de la variable %1 avec la valeur %2 dans le processus %0 - Aucune donnée trouvée dans la requête.'
 );
  
 insert into flow_messages( fmsg_message_key, fmsg_lang, fmsg_message_content )
  values (
 'var_exp_sql_other',
-'en',
-'Error setting %2 process variable %1 in process id %0.  SQL error shown in event log.'
+'fr',
+'Ereur lors de la mise à jour de la variable %1 avec la valeur %2 dans le processus %0.  Erreur SQL indiquée dans le journal des événements.'
 );
  
 insert into flow_messages( fmsg_message_key, fmsg_lang, fmsg_message_content )
  values (
 'var_exp_sql_too_many_rows',
-'en',
-'Error setting %2 process variable %1 in process id %0.  Query returns multiple rows.'
+'fr',
+'Ereur lors de la mise à jour de la variable %1 avec la valeur %2 dans le processus %0.  La requête renvoie plusieurs lignes.'
 );
- 
- /* Plug-ins */
+
+
+/* Plug-ins */
 insert into flow_messages( fmsg_message_key, fmsg_lang, fmsg_message_content )
  values (
 'plugin-model-no-version',
-'en',
-'Version not defined.'
+'fr',
+'La version n''est pas définie.'
 );
 
 insert into flow_messages( fmsg_message_key, fmsg_lang, fmsg_message_content )
  values (
 'plugin-parsing-json-variables',
-'en',
-'Error during parsing process variables.'
+'fr',
+'Erreur lors de l''analyse des variables du processus.'
 );
 
 insert into flow_messages( fmsg_message_key, fmsg_lang, fmsg_message_content )
  values (
 'plugin-route-not-define',
-'en',
-'Gateway is not define for routing.'
+'fr',
+'La passerelle n''est pas définie pour le routage.'
 );
 
 insert into flow_messages( fmsg_message_key, fmsg_lang, fmsg_message_content )
  values (
 'plugin-gateway-not-exist',
-'en',
-'Gateway define does not exists for this flow.'
+'fr',
+'La définition de la passerelle n''existe pas pour ce flux.'
 );
 
 insert into flow_messages( fmsg_message_key, fmsg_lang, fmsg_message_content )
  values (
 'plugin-no-instance-subflow-id',
-'en',
-'Unable to get Flow Instance Id and or subflow Id to manage the step.'
+'fr',
+'Impossible d''obtenir l''Id de l''Instance de flux et ou l''Id du sous-flux pour gérer l''étape.'
 );
 
 insert into flow_messages( fmsg_message_key, fmsg_lang, fmsg_message_content )
  values (
 'plugin-wrong-variable-number',
-'en',
-'Wrong number of APEX item(s) or process variable(s).'
+'fr',
+'Mauvais nombre d''élément(s) APEX ou de variable(s) de processus.'
 );
 
 insert into flow_messages( fmsg_message_key, fmsg_lang, fmsg_message_content )
  values (
 'plugin-wrong-variable-type',
-'en',
-'One or more process variable(s) are a different type than the one defined in the JSON.'
+'fr',
+'Une ou plusieurs variables de processus sont d''un type différent de celui défini dans le JSON.'
 );
 
 insert into flow_messages( fmsg_message_key, fmsg_lang, fmsg_message_content )
  values (
 'plugin-variable-not-a-number',
-'en',
-'%0 is not a valid number.'
+'fr',
+'%0 n''est pas un nombre valide.'
 );
 
 insert into flow_messages( fmsg_message_key, fmsg_lang, fmsg_message_content )
  values (
 'plugin-variable-not-a-date',
-'en',
-'%0 is not a valid date.'
+'fr',
+'%0 n''est pas une date valide.'
 );
 
 insert into flow_messages( fmsg_message_key, fmsg_lang, fmsg_message_content )
  values (
 'plugin-modeler-id-not-dound',
-'en',
-'No data found. Check if Diagram with given ID exists.'
+'fr',
+'Aucune donnée trouvée. Vérifiez si le Diagramme avec l''ID fourni existe.'
 );
 
 insert into flow_messages( fmsg_message_key, fmsg_lang, fmsg_message_content )
  values (
 'plugin-unexpected-error',
-'en',
-'Unexpected error, please contact your administrator.'
+'fr',
+'Erreur inattendue, veuillez contacter votre administrateur.'
 );
 
 insert into flow_messages( fmsg_message_key, fmsg_lang, fmsg_message_content )
  values (
 'plugin-diagram-not-parsable',
-'en',
-'Diagram could not be parsed.<br />Please review your diagram to ensure that it is supported.'
+'fr',
+'Le diagramme n''a pas pu être analysé.<br />Veuillez examiner votre diagramme pour vous assurer qu''il est pris en charge.'
 );
 
 insert into flow_messages( fmsg_message_key, fmsg_lang, fmsg_message_content )
  values (
 'plugin-diagram-saved',
-'en',
-'Changes saved!'
+'fr',
+'Changements enregistrés !'
 );
 
 insert into flow_messages( fmsg_message_key, fmsg_lang, fmsg_message_content )
  values (
 'plugin-diagram-has-changed',
-'en',
-'Model has changed. Discard changes?'
+'fr',
+'Le modlèle a changé, annuler les changements?'
 );
-
