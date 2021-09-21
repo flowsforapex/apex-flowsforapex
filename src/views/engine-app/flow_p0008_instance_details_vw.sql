a28f1cab6697a3f46b81e8e0b46bb0690418fbc6
Louis Moreaux <moreaux.louis@gmail.com>
2021-09-21 09:39:00 +0200

    Display data in the user time zone
    Related to #300

--- src/views/engine-app/flow_p0008_instance_details_vw.sql
+++ src/views/engine-app/flow_p0008_instance_details_vw.sql
@@ -5,8 +5,8 @@ as
        prcs_name,
        dgrm_name || ' - Version ' || dgrm_version as flow_diagram,
        prcs_status as status,
-       prcs_init_ts as initialized_on,
-       prcs_last_update as last_update_on,
+       prcs_init_ts at time zone sessiontimezone as initialized_on,
+       prcs_last_update at time zone sessiontimezone as last_update_on,
        prcs_business_ref as business_reference
   from flow_instances_vw
 with read only;
