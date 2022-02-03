46e1d4d558fc25afd8a23bb50856d3223e272399
commi235 <moritz.klein@it-twins.de>
2022-02-03 20:04:44 +0100

    migration script finalized

--- src/data/install_default_config_data.sql
+++ src/data/install_default_config_data.sql
@@ -1,11 +1,16 @@
-insert into flow_configuration (cfig_key, cfig_value) values ('logging_level', 'standard');
-insert into flow_configuration (cfig_key, cfig_value) values ('logging_hide_userid', 'false');
-insert into flow_configuration (cfig_key, cfig_value) values ('logging_language','en');
-insert into flow_configuration (cfig_key, cfig_value) values ('engine_app_mode','production');
--- put new systems into strict mode for step keys (migrated systems are in legacy mode)
-insert into flow_configuration (cfig_key, cfig_value) values ('duplicate_step_prevention','strict');
-insert into flow_configuration (cfig_key, cfig_value) values ('version_initial_installed','22.1');
-insert into flow_configuration (cfig_key, cfig_value) values ('version_now_installed','22.1');
-insert into flow_configuration (cfig_key, cfig_value) values ('default_workspace', 'FLOWS4APEX');
-insert into flow_configuration (cfig_key, cfig_value) values ('default_email_sender', '');
-insert into flow_configuration (cfig_key, cfig_value) values ('timer_max_cycles','1000');
+PROMPT >> Inital System Configuration for new systems
+begin
+  insert into flow_configuration (cfig_key, cfig_value) values ('logging_level', 'standard');
+  insert into flow_configuration (cfig_key, cfig_value) values ('logging_hide_userid', 'false');
+  insert into flow_configuration (cfig_key, cfig_value) values ('logging_language','en');
+  insert into flow_configuration (cfig_key, cfig_value) values ('engine_app_mode','production');
+  -- put new systems into strict mode for step keys (migrated systems are in legacy mode)
+  insert into flow_configuration (cfig_key, cfig_value) values ('duplicate_step_prevention','strict');
+  insert into flow_configuration (cfig_key, cfig_value) values ('version_initial_installed','22.1');
+  insert into flow_configuration (cfig_key, cfig_value) values ('version_now_installed','22.1');
+  insert into flow_configuration (cfig_key, cfig_value) values ('default_workspace', 'FLOWS4APEX');
+  insert into flow_configuration (cfig_key, cfig_value) values ('default_email_sender', '');
+  insert into flow_configuration (cfig_key, cfig_value) values ('timer_max_cycles','1000');
+  commit;
+end;
+/
