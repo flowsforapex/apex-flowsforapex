8989b08dfa11bc9254472bedeff4b8643928dad3
commi235 <moritz.klein@it-twins.de>
2022-06-01 19:18:15 +0200

    Final fix for reworked parser

--- src/views/engine-app/flow_p0007_vw.sql
+++ src/views/engine-app/flow_p0007_vw.sql
@@ -16,26 +16,18 @@ as
          , objt.objt_bpmn_id as calling_objt
          , dgrm.dgrm_name || ' (' || dgrm.dgrm_version || ')' as breadcrumb
          , 1 as sub_prcs_insight
-      from ( select obat_objt_id
-                  , obat_key
-                  , obat_vc_value
-               from flow_object_attributes
-           )
-     pivot ( min(obat_vc_value) for obat_key in
-             ( 'apex:calledDiagram' as called_diagram
-             , 'apex:calledDiagramVersionSelection' as called_diagram_version_selection
-             , 'apex:calledDiagramVersion' as called_diagram_version
-             )
-           ) attr
+      from flow_objects objt
       join flow_diagrams dgrm
-        on ( attr.called_diagram = dgrm.dgrm_name
-             and case when attr.called_diagram_version_selection = 'latestVersion' and (dgrm.dgrm_status = 'released' or (dgrm.dgrm_status = 'draft' and dgrm.dgrm_version = '0')) then 1
-                      when attr.called_diagram_version_selection = 'namedVersion' and dgrm.dgrm_version = attr.called_diagram_version then 1
-                      else 0
-                  end = 1
+        on objt.objt_attributes."apex"."calledDiagram" = dgrm.dgrm_name
+       and (  (   objt.objt_attributes."apex"."calledDiagramVersionSelection" = 'latestVersion'
+              and ( dgrm.dgrm_status = 'released'
+                  or ( dgrm.dgrm_status = 'draft' and dgrm.dgrm_version = '0' )
+                  )
+              )
+           or ( objt.objt_attributes."apex"."calledDiagramVersionSelection" = 'namedVersion'
+              and dgrm.dgrm_version = objt.objt_attributes."apex"."calledDiagramVersion"
+              )
            )
-      join flow_objects objt
-        on attr.obat_objt_id = objt.objt_id
   )
   select dgrm.dgrm_content
        , dgrm.dgrm_id
@@ -46,5 +38,5 @@ as
        , connect_by_root(dgrm.dgrm_id) as root_dgrm
     from diagrams dgrm
    start with dgrm.calling_dgrm is null
- connect by dgrm.calling_dgrm = prior dgrm.dgrm_id  
+ connect by dgrm.calling_dgrm = prior dgrm.dgrm_id
 with read only;
