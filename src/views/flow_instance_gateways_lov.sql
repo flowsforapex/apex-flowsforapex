3d00ed06dcdee71fe7263fdb11b38b1a04bda5f4
Richard Allen <rallen2010@gmail.com>
2022-08-24 16:54:57 +0100

    fix associations #498

--- src/views/flow_instance_gateways_lov.sql
+++ src/views/flow_instance_gateways_lov.sql
@@ -13,5 +13,8 @@ as
     join flow_objects obj
       on obj.objt_dgrm_id = ins.dgrm_id
    where obj.objt_tag_name in ('bpmn:exclusiveGateway', 'bpmn:inclusiveGateway')
-     and ( select count(*) from flow_connections conn where conn.conn_src_objt_id = obj.objt_id ) > 1
+     and ( select count(*) 
+           from flow_connections conn 
+           where conn.conn_src_objt_id = obj.objt_id 
+           and conn.conn_tag_name = 'bpmn:sequenceFlow') > 1
 with read only;
