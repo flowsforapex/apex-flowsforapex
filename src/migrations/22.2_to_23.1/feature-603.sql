72d573ce03636d5a39df0988e8ac7e0ac350dbe1
commi235 <moritz.klein@it-twins.de>
2023-04-28 15:33:17 +0200

    flow_subfows index for process and diagram

--- src/migrations/22.2_to_23.1/feature-603.sql
+++ src/migrations/22.2_to_23.1/feature-603.sql
@@ -1,5 +1,6 @@
-PROMPT >> Performance Improvements for Instance Numbers
+PROMPT >> Performance Improvements for Instance Numbers and General Performance Enhancements
 
+PROMPT >> Instance Numbers
 create index flow_prcs_dgrm_status_ix
   on flow_processes (prcs_dgrm_id, prcs_status);
 
@@ -10,3 +11,6 @@ create index flow_prdg_dgrm_prcs_ix
   on flow_instance_diagrams (prdg_prcs_id, prdg_dgrm_id);
 
 PROMPT >> Updated View flow_p0002_diagrams_vw will get installed via common_db_objects.sql
+
+PROMPT >> General
+create index flow_sbfl_dgrm_prcs_ix on flow_subflows( sbfl_dgrm_id, sbfl_prcs_id );
