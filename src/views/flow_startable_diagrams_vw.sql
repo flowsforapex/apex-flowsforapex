6f3fd5ef97b75dc479747f5e1414950c21c57d39
Louis Moreaux <moreaux.louis@gmail.com>
2023-06-05 11:26:26 +0200

    Update flow_startable_diagrams_vw.sql
    Add suffix _vw and replace nvl2 by coalesce

--- src/views/flow_startable_diagrams_vw.sql
+++ src/views/flow_startable_diagrams_vw.sql
@@ -1,9 +1,10 @@
 /* 
--- Flows for APEX - flow_startable_diagrams.sql
+-- Flows for APEX - flow_startable_diagrams_vw.sql
 -- 
 -- (c) Copyright Oracle Corporation and / or its affiliates, 2023.
 --
 -- Created    05-Apr-2023  Richard Allen (Oracle)
+-- Edited     05-Jun-2023  Louis Moreaux, Insum
 --
 -- This view shows any process diagrams that are marked as startable, along with users and groups that can start them
 -- and excluded uses who cannot start them (takes precedence over positive grants in potentialStartingGroups).
@@ -11,7 +12,7 @@
 --  -- Note this View can only be queried inside an APEX session
 --
 */
-create or replace view flow_startable_diagrams 
+create or replace view flow_startable_diagrams_vw
 as 
 select dgrm.dgrm_id
      , dgrm.dgrm_name
@@ -19,9 +20,9 @@ select dgrm.dgrm_id
      , dgrm.dgrm_status
      , objt.objt_name process_name
      , objt.objt_bpmn_id process_bpmn_id
-     , nvl2(objt.objt_attributes."apex"."potentialStartingUsers", flow_settings.get_vc2_expression(pi_expr=> objt.objt_attributes."apex"."potentialStartingUsers"),null) potential_starting_users
-     , nvl2(objt.objt_attributes."apex"."potentialStartingGroups", flow_settings.get_vc2_expression(pi_expr=> objt.objt_attributes."apex"."potentialStartingGroups"),null) potential_starting_groups
-     , nvl2(objt.objt_attributes."apex"."excludedStartingUsers", flow_settings.get_vc2_expression(pi_expr=> objt.objt_attributes."apex"."excludedStartingUsers"),null) excluded_starting_users
+     , coalesce(objt.objt_attributes."apex"."potentialStartingUsers", flow_settings.get_vc2_expression(pi_expr=> objt.objt_attributes."apex"."potentialStartingUsers"),null) potential_starting_users
+     , coalesce(objt.objt_attributes."apex"."potentialStartingGroups", flow_settings.get_vc2_expression(pi_expr=> objt.objt_attributes."apex"."potentialStartingGroups"),null) potential_starting_groups
+     , coalesce(objt.objt_attributes."apex"."excludedStartingUsers", flow_settings.get_vc2_expression(pi_expr=> objt.objt_attributes."apex"."excludedStartingUsers"),null) excluded_starting_users
   from flow_diagrams dgrm
   join flow_objects objt
     on dgrm.dgrm_id = objt.objt_dgrm_id
