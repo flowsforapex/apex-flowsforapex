c458cbdd82b2eec92197da8dcc24d6beea4b2868
Dennis Amthor <dennis.amthor@mt-ag.com>
2024-10-17 16:44:10 +0200

    Added helper script to run at the end of supporting objects

---
+++ helpers/release_tutorials.sql
@@ -0,0 +1,10 @@
+PROMPT >> Release tutorials that need to be release to run properly
+PROMPT >> =============================
+
+begin
+  flow_admin_api.release_diagram (pi_dgrm_name =>'Tutorial 5e - ship Goods (Called by Tutorial 5d)', pi_dgrm_version => '24.1');
+end;
+/
+
+PROMPT >> Finished releasing tutorials
+PROMPT >> =============================
