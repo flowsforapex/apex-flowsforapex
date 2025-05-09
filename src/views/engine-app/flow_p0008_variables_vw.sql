d871d1f836d072550420006b858203d2b06ce6b6
Dennis Amthor <dennis.amthor@mt-ag.com>
2025-05-09 14:50:46 +0200

    Fixed/aligned conversion of proc var datatypes

--- src/views/engine-app/flow_p0008_variables_vw.sql
+++ src/views/engine-app/flow_p0008_variables_vw.sql
@@ -17,9 +17,9 @@ as
             when prov_var_vc2  is not null then prov_var_vc2
             when prov_var_num  is not null then cast(prov_var_num as varchar2(4000))
             when prov_var_date is not null then to_char(prov_var_date, v('APP_DATE_TIME_FORMAT'))
-            when prov_var_clob is not null then cast(dbms_lob.substr(prov_var_clob, 4000) as  varchar2(4000))
-            when prov_var_tstz is not null then to_char(prov_var_tstz, 'YYYY-MM-DD HH24:MI:SS TZR')
-            when prov_var_json is not null then to_char(prov_var_json)
+            when prov_var_clob is not null then cast(dbms_lob.substr(prov_var_clob, 1000) as varchar2(4000))
+            when prov_var_tstz is not null then to_char(prov_var_tstz, v('NLS_TIMESTAMP_TZ_FORMAT'))
+            when prov_var_json is not null then cast(dbms_lob.substr(prov_var_json, 1000) as varchar2(4000))
         end as prov_var_value,
         case when instr(prov_var_name, ':route') > 0 then 'true' else 'false' end is_gateway_route
     from flow_instance_variables_vw
--- src/views/engine-app/flow_p0013_variable_log_vw.sql
+++ src/views/engine-app/flow_p0013_variable_log_vw.sql
@@ -8,11 +8,11 @@ as
        , lgvr_timestamp at time zone sessiontimezone as lgvr_timestamp
        , lgvr_var_type
        , case
-           when lgvr_var_vc2 is not null then lgvr_var_vc2
-           when lgvr_var_num is not null then cast (lgvr_var_num as varchar2(4000))
+           when lgvr_var_vc2  is not null then lgvr_var_vc2
+           when lgvr_var_num  is not null then cast(lgvr_var_num as varchar2(4000))
            when lgvr_var_date is not null then to_char(lgvr_var_date, v('APP_DATE_TIME_FORMAT'))
+           when lgvr_var_clob is not null then cast(dbms_lob.substr(lgvr_var_clob, 1000) as varchar2(4000))
            when lgvr_var_tstz is not null then to_char(lgvr_var_tstz, v('NLS_TIMESTAMP_TZ_FORMAT'))
-           when lgvr_var_clob is not null then '[clob]'
            when lgvr_var_json is not null then cast(dbms_lob.substr(lgvr_var_json, 4000) as  varchar2(4000))
          end as lgvr_value
     from flow_variable_event_log
