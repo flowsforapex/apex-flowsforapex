2dcf97abc2ac86b49053b3aba6c31e0debcde43a
Dennis Amthor <dennis.amthor@mt-ag.com>
2025-06-10 11:04:41 +0200

    Added description columns, moved icon definitions,

--- src/views/flow_diagrams_vw.sql
+++ src/views/flow_diagrams_vw.sql
@@ -2,8 +2,16 @@ create or replace view flow_diagrams_vw
 as
   select dgrm.dgrm_id
        , dgrm.dgrm_name
+       , dgrm.dgrm_short_description
+       , dgrm.dgrm_description
        , dgrm.dgrm_version
        , dgrm.dgrm_status
+       , case dgrm_status
+          when 'draft'      then 'fa fa-wrench'
+          when 'released'   then 'fa fa-check'
+          when 'deprecated' then 'fa fa-ban'
+          when 'archived'   then 'fa fa-archive'
+         end as dgrm_status_icon
        , dgrm.dgrm_category
        , dgrm.dgrm_last_update
        , dgrm.dgrm_content
--- src/views/flow_startable_diagrams_vw.sql
+++ src/views/flow_startable_diagrams_vw.sql
@@ -6,6 +6,7 @@
 -- Created    05-Apr-2023  Richard Allen (Oracle)
 -- Edited     05-Jun-2023  Louis Moreaux, Insum
 -- Edited     07-Oct-2024  Dennis Amthor, Hyand Solutions GmbH
+-- Edited     06-Jun-2025  Dennis Amthor, Hyand Solutions GmbH
 --
 -- This view shows any process diagrams that are marked as startable, along with users and groups that can start them
 -- and excluded uses who cannot start them (takes precedence over positive grants in potentialStartingGroups).
@@ -17,14 +18,18 @@ create or replace view flow_startable_diagrams_vw
 as 
 select dgrm.dgrm_id
      , dgrm.dgrm_name
+     , dgrm.dgrm_short_description
+     , dgrm.dgrm_description
      , dgrm.dgrm_version
      , dgrm.dgrm_status
+     , dgrm.dgrm_status_icon
+     , dgrm.dgrm_last_update
      , objt.objt_name process_name
      , objt.objt_bpmn_id process_bpmn_id
      , nvl2(objt.objt_attributes."apex"."potentialStartingUsers", flow_settings.get_vc2_expression(pi_expr=> objt.objt_attributes."apex"."potentialStartingUsers"),null) potential_starting_users
      , nvl2(objt.objt_attributes."apex"."potentialStartingGroups", flow_settings.get_vc2_expression(pi_expr=> objt.objt_attributes."apex"."potentialStartingGroups"),null) potential_starting_groups
      , nvl2(objt.objt_attributes."apex"."excludedStartingUsers", flow_settings.get_vc2_expression(pi_expr=> objt.objt_attributes."apex"."excludedStartingUsers"),null) excluded_starting_users
-  from flow_diagrams dgrm
+  from flow_diagrams_vw dgrm
   join flow_objects objt
     on dgrm.dgrm_id = objt.objt_dgrm_id
    and objt.objt_attributes."apex"."isStartable" = 'true'
